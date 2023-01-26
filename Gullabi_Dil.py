fad6712320615f0b411a9f050d76854ea203ba69
DEBARGHYA CHAKRAVARTY <83878346+Deba951@users.noreply.github.com>
2023-01-26 10:12:03 +0530

    Add files via upload

---
+++ Gullabi_Dil.py
@@ -0,0 +1,34 @@
+#Part 2
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor('black')
+t.hideturtle()
+t.goto(0,-10)
+
+t.pensize(3)
+t.color('red')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(178)
+t.end_fill()
+
+t.penup()
+t.goto(-75,130)
+t.pendown()
+t.color('white')
+t.write("I Love You",font=("Verdana",25,"bold"))
+
+t.penup()
+t.goto(-220,-180)
+t.pendown()
+t.color('white')
+t.write("NAME",font=("Verdana",30,"bold"))
+
+turtle.done()
